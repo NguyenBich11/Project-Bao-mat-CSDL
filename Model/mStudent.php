9c5c22f8a1b1b98e18743b23b4cef92a93b13c2e
Camtien25 <142108407+Camtien25@users.noreply.github.com>
2025-03-13 00:59:09 +0700

    mStudent.php

--- Model/mStudent.php
+++ Model/mStudent.php
@@ -32,6 +32,28 @@
         }
 
         public function insertStudent() {
+             public function insertStudent($mssv, $hoTen, $ngaySinh, $gioiTinh, $lopDN) {
+        $p = new clsConnect();
+        $conn = $p->mOpen();
+        // Kiểm tra MSSV đã tồn tại chưa
+        $queryCheck = "SELECT mssv FROM sinhvien WHERE mssv = '$mssv'";
+        $resultCheck = $conn->query($queryCheck);
+
+        if ($resultCheck->num_rows > 0) {
+            return "MSSV đã tồn tại!";
+        } else {
+            // Nếu MSSV chưa tồn tại, thêm sinh viên
+            $queryInsert = "INSERT INTO sinhvien (mssv, hoten, ngaysinh, gioitinh, lopdanhnghia) 
+                            VALUES ('$mssv', '$hoTen', '$ngaySinh', '$gioiTinh', '$lopDN')";
+            
+            if ($conn->query($queryInsert)) {
+                return "Xem danh sách sinh viên !";
+            } else {
+                return "Sinh viên không có trong danh sách!";
+            }
+        }
+    }
+            
 
         }
 
